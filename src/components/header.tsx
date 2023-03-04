286dc8725d0031606607985570c17423cb1155aa
Nekotora <i@flag.moe>
2023-03-04 21:43:58 +0800

    fix animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,27 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { flushSync } from 'react-dom'
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
 import { getRandom } from "../helper/helper";
 
+let mount = false
 export function SiteHeader() {
   const location = useLocation();
   const { t, i18n } = useTranslation();
   let isActive = false;
 
-  if(location.pathname == '' || location.pathname == '/') {
+  if (location.pathname == '' || location.pathname == '/') {
     isActive = true;
   }
 
   const [text, setText] = useState([
-    ['...', '...'], // next one
-    ['...', '...'] // last one
+    ['...', '...'], // new one
+    ['...', '...'] // old one
   ]);
-  const [timer, setTimer] = useState<number | null>(null);
-  const text1BoxRef = useRef<any>(null);
-  const text2BoxRef = useRef<any>(null);
-  const text1Ref = useRef<any>(null);
-  const text2Ref = useRef<any>(null);
+  const text1BoxRef = useRef<HTMLSpanElement>(null);
+  const text2BoxRef = useRef<HTMLSpanElement>(null);
+  const text1Ref = useRef<HTMLSpanElement>(null);
+  const text2Ref = useRef<HTMLSpanElement>(null);
 
   const text1 = [
     t('sloganText1.Livestream software'),
@@ -34,38 +35,28 @@ export function SiteHeader() {
   ];
 
   useEffect(() => {
-    if(timer) return;
     const t = setInterval(() => {
+      const newA = text1[getRandom(0, text1.length - 1)];
+      const newB = text2[getRandom(0, text2.length - 1)];
+
       setText((text) => {
-        return [
-          [
-            text1[getRandom(0, text1.length-1)],
-            text2[getRandom(0, text2.length-1)]
-          ],
-          text[0]
-        ]
+        return [[newA, newB], text[0]]
       });
-      const replay = (el) => {
-        el.className = 'text-item';
-        let name = el.getAttribute('data-animate');
-        window.requestAnimationFrame(function(time) {
-          window.requestAnimationFrame(function(time) {
-            el.className = name;
-          });
-        });
-      }
-      console.log(text1BoxRef.current.children);
-      Array.from(text1BoxRef.current.children).forEach(replay);
-      Array.from(text2BoxRef.current.children).forEach(replay);
     }, 1000)
-    setTimer(t);
-    
+    return () => clearInterval(t)
   }, []);
 
-  useEffect(() => {
-    if(!text1BoxRef.current || !text1Ref.current || !text2BoxRef.current || !text2Ref.current) return;
-    text1BoxRef.current.style.width = text1Ref.current.offsetWidth + 'px';
-    text2BoxRef.current.style.width = text2Ref.current.offsetWidth + 'px';
+  useLayoutEffect(() => {
+    const exiting: Keyframe[] = [{ transform: 'translateY(0px)', opacity: 1 }, { transform: 'translateY(10px)', opacity: 0 }];
+    const entering: Keyframe[] = [{ transform: 'translateY(-10px)', opacity: 0 }, { transform: 'translateY(0px)', opacity: 1 }];
+
+    text1BoxRef.current!.children[0].animate(exiting, { duration: 300, easing: 'ease-in-out', fill: 'forwards' }).play()
+    text1BoxRef.current!.children[1].animate(entering, { duration: 300, easing: 'ease-in-out', fill: 'forwards' }).play()
+
+    text2BoxRef.current!.children[0].animate(exiting, { duration: 300, easing: 'ease-in-out', fill: 'forwards' }).play()
+    text2BoxRef.current!.children[1].animate(entering, { duration: 300, easing: 'ease-in-out', fill: 'forwards' }).play()
+    text1BoxRef.current!.style.width = text1Ref.current!.offsetWidth + 'px';
+    text2BoxRef.current!.style.width = text2Ref.current!.offsetWidth + 'px';
   }, [text])
 
   return (
@@ -74,13 +65,13 @@ export function SiteHeader() {
       <div className="container">
         <div className="slogan">
           <span className="text-box" ref={text1BoxRef}>
-            <b className={`animate-exiting`} data-animate='animate-exiting'>{text[0][0]}</b>
-            <b className={`animate-entering`} data-animate='animate-entering' ref={text1Ref}>{text[1][0]}</b>
+            <b>{text[1][0]}</b>
+            <b ref={text1Ref}>{text[0][0]}</b>
           </span>
           <span className="dot">for</span>
           <span className="text-box" ref={text2BoxRef}>
-            <b className={`animate-exiting`} data-animate='animate-exiting'>{text[0][1]}</b>
-            <b className={`animate-entering`} data-animate='animate-entering' ref={text2Ref}>{text[1][1]}</b>
+            <b>{text[1][1]}</b>
+            <b ref={text2Ref}>{text[0][1]}</b>
           </span>
         </div>
       </div>
--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,9 +1,22 @@
 export function PricingPage() {
   return (
     <>
-      <div className="page">
+      <div className="page price-page">
       <div className="container">
           <h1>Pricing</h1>
+        </div>
+        <div className="container">
+          <div className="price-lists">
+            <div className="list">
+              <div className="price">Free*</div>
+              <div className="features">
+                <div className="item">list1</div>
+                <div className="item">list2</div>
+                <div className="item">list3</div>
+              </div>
+            </div>
+            <div className="list"></div>
+          </div>
         </div>
       </div>
     </>
